dc9ba8fc4f6c09d8a146c51c9713919f2e0be74a
TwoOnefour <lys214412@gmail.com>
2023-04-07 15:03:10 +0800

    time.sleep()修改

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ session = requests.Session()
 userprojectid = "" # 填入自己的，下面略
 userid = ""
 username = ""
+sleeptime = 60
 headers = {
     "Host": "weiban.mycourse.cn",
     "Accept": "*/*",
@@ -80,7 +81,7 @@ def get_course_id(session):
                 "referer": "https://weiban.mycourse.cn/",
                 "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.34(0x1800222f) NetType/WIFI Language/en",
                 })
-                time.sleep(60)  # 这里要等一下，不然刷不了
+                time.sleep(sleeptime)  # 这里要等一下，不然刷不了
                 finish_course(session, j["userCourseId"], parseResult.query.split("&")[3][12:])
 
 
