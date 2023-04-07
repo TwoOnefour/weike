5478fe2f92e2d4dddf605c34ee97f2adea583ee4
TwoOnefour <lys214412@gmail.com>
2023-04-08 02:00:33 +0800

    接入chatGPT自动做题

--- cancel_bind.py
+++ cancel_bind.py
@@ -1,18 +1,79 @@
 import os.path
-
+import time
 import requests
+import random
 
-
-
-def cancel_bind(session, userid):
-    session.post("https://weiban.mycourse.cn/pharos/my/cancelBindWechat.do?timestamp={}".format(int(time.time())), verify=False, data={
+def login(session):
+    global userid, username, userprojectid
+    if os.path.exists("./token.txt"):
+        with open("./token.txt", "r") as f:
+            result = f.readlines()
+            userid = result[0].strip("\n")
+            username = result[1].strip("\n")
+            userprojectid = result[2].strip("\n")
+        return
+    openid = ""
+    for i in range(28):
+        if (i + random.randint(65, 91)) % 2 == 0:
+            openid += chr(random.randint(65, 90))
+        else:
+            openid += chr(random.randint(97, 122))
+    result = session.post("https://weiban.mycourse.cn/pharos/login/bindWechat.do?timestamp={}".format(int(time.time())),verify=False,data={
+        "sno": sno,
+        "password": sno,
+        "openid": openid,  # 这个可以为假，位数正确即可 "oeNCVuNUTOo9YSuBDVasVYCY0vsB"
+        "tenantCode":43000010,
+        "type":1
+    })
+    result = json.loads(result.text)
+    try:
+        if result["msg"] == "该用户已经绑定过微信！":
+            print("请解绑微信和账号的关联，进入安全微伴-->我的-->解除微信绑定")
+            return
+    except Exception as e:
+        pass
+    userid = result["data"]["userId"]
+    username = result["data"]["userName"]
+    result = session.post("https://weiban.mycourse.cn/pharos/index/listStudyTask.do?timestamp={}".format(int(time.time())), verify=False, data={
         "userId": userid,
         "tenantCode": 43000010,
         "limit": 3
     })
+    result = json.loads(result.text)
+    userprojectid = result["data"][0]["userProjectId"]
+    with open("./token.txt", "w") as f:
+        f.write(userid + "\n")
+        f.write(username + "\n")
+        f.write(userprojectid)
+def cancel_bind(session, userid):
+    print(session.post("https://weiban.mycourse.cn/pharos/my/cancelBindWechat.do?timestamp={}".format(int(time.time())), verify=False, data={
+        "userId": userid,
+        "tenantCode": 43000010,
+        "limit": 3
+    },headers = {
+    "Host": "weiban.mycourse.cn",
+    "Accept": "*/*",
+    "Connection": "keep-alive",
+    # "X-Token":"63501030-78bc-4811-b191-e56e52e5982a",
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.34(0x1800222f) NetType/WIFI Language/en",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Referer": "https://weiban.mycourse.cn/index.html",
+    "Accept-Encoding": "gzip, deflate, br"
+}))
+
 
 if __name__ == "__main__":
-    if os.path.exists("./token.txt", "r"):
-        with open("./token.txt") as f:
-            userid = f.readlines()[0]
-    cancel_bind(requests.Session(), userid)
+    userid = ""
+    username = ""
+    userprojectid = ""
+    session = requests.Session()
+    if os.path.exists("./token.txt"):
+        with open("./token.txt", "r") as f:
+            result = f.readlines()
+            userid = result[0]
+            username = result[0]
+            userprojectid = result[0]
+    else:
+        sno = "" # 你的学号
+    login(session)
+    cancel_bind(session, userid)
--- main.py
+++ main.py
@@ -6,26 +6,9 @@ import time
 import json
 import urllib
 import urllib3
-urllib3.disable_warnings()
-session = requests.Session()
-# sno = "" # 自己的学号
-sno = input("请输入学号：")
+from exam import *
 
-userprojectid = ""
-userid = ""
-username = ""
-sleeptime = 30  # sleeptime
-headers = {
-    "Host": "weiban.mycourse.cn",
-    "Accept": "*/*",
-    "Connection": "keep-alive",
-    # "X-Token":"63501030-78bc-4811-b191-e56e52e5982a",
-    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.34(0x1800222f) NetType/WIFI Language/en",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Referer": "https://weiban.mycourse.cn/index.html",
-    "Accept-Encoding": "gzip, deflate, br"
-}
-session.headers.update(headers)
+
 # session.cookies.update({
 #     "Hm_lpvt_05399ccffcee10764eab39735c54698f":"1680840829",
 #     "m_lvt_05399ccffcee10764eab39735c54698f":"1680828613,1680832674,1680833646,1680835279",
@@ -120,6 +103,7 @@ def finish_course(session, userCourseId, token):
 },cookies={"SERVERID":session.cookies.get("SERVERID")}, verify=False)
     # print(1)
     # print(result)
+
 def login(session):
     global userid, username, userprojectid
     if os.path.exists("./token.txt"):
@@ -143,9 +127,12 @@ def login(session):
         "type":1
     })
     result = json.loads(result.text)
-    if result["msg"] == "该用户已经绑定过微信!":
-        print("请解绑微信和账号的关联，进入安全微伴-->我的-->解除微信绑定")
-        return
+    try:
+        if result["msg"] == "该用户已经绑定过微信！":
+            print("请解绑微信和账号的关联，进入安全微伴-->我的-->解除微信绑定")
+            return
+    except Exception as e:
+        pass
     userid = result["data"]["userId"]
     username = result["data"]["userName"]
     result = session.post("https://weiban.mycourse.cn/pharos/index/listStudyTask.do?timestamp={}".format(int(time.time())), verify=False, data={
@@ -160,6 +147,75 @@ def login(session):
         f.write(username + "\n")
         f.write(userprojectid)
 
-login(session)
-get_course_id(session)
-cancel_bind(session, userid)  # 如果没有解绑请单独运行cancel_bind
+# get_course_id(session)
+def finishExam(session):
+    # from exam import *
+    # global userid, username, userprojectid
+    result1 = session.post("https://weiban.mycourse.cn/pharos/exam/listPlan.do?timestamp={}".format(int(time.time()) + 1),verify=False,data={
+        "userProjectId":userprojectid,
+        "tenantCode":43000010,
+        "userId":userid
+    })
+    userExamPlanId = json.loads(result1.text)["data"]["userExamPlanId"]
+    examPlanId = json.loads(result1.text)["data"]["id"]
+    result = exam(session, userid, userExamPlanId)
+
+
+    data = {
+        "examPlanId":examPlanId,
+        "userExamPlanId":userExamPlanId,
+        "useTime":0,
+        "answerIds":"",
+        "tenantCode":43000010,
+        "userId":userid,
+        "questionId":"",
+    }
+    for i in result["data"]["questionList"]:
+        # str1 = ""
+        answer = ""
+        type = i["typeLabel"]
+        for option in i["optionList"]:
+            answer += "选项id：'{}'，选项内容：'{}'\n".format(option["id"], option["content"])
+        str1 = "{}, 问题：{}\n{}".format(type, i["title"], answer)
+        answerid = send_to_chatGPT(str1)
+        data["questionId"] = i["id"]
+        data["answerIds"] = answerid
+        data["useTime"] = int(time.time()) - int(session.cookies.get("SERVERID").split("|")[1]) + 1,
+        result = session.post("https://weiban.mycourse.cn/pharos/exam/recordQuestion.do?timestamp={}".format(int(time.time()) + 1), data=data)
+        # print(result.text)
+
+    session.get("https://weiban.mycourse.cn/static/img/quest-op-order.efa5bac.png", verify=False)
+    result = session.post("https://weiban.mycourse.cn/pharos/exam/submitPaper.do?timestamp={}".format(int(time.time()) + 1), data={
+    "userExamPlanId":userExamPlanId,
+    "tenantCode":43000010,
+    "userId":userid
+    })
+    print(result.text)
+# print(result.text)
+# send_to_chatGPTlogin(session)
+
+if __name__ == "__main__":
+    urllib3.disable_warnings()
+    session = requests.Session()
+    # sno = "" # 自己的学号
+    if not os.path.exists("./token.txt"):
+        sno = input("请输入学号：")
+    userprojectid = ""
+    userid = ""
+    username = ""
+    sleeptime = 30  # sleeptime
+    headers = {
+        "Host": "weiban.mycourse.cn",
+        "Accept": "*/*",
+        "Connection": "keep-alive",
+        # "X-Token":"63501030-78bc-4811-b191-e56e52e5982a",
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.34(0x1800222f) NetType/WIFI Language/en",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Referer": "https://weiban.mycourse.cn/index.html",
+        "Accept-Encoding": "gzip, deflate, br"
+    }
+    session.headers.update(headers)
+    login(session)
+    get_course_id(session)
+    # finishExam(session)  #  如果需要gpt作答，请取消此行注释
+    cancel_bind(session, userid)  # 如果没有解绑请单独运行cancel_bind
